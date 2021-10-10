b824dd76453797c7a67db629933069c484e37c5c
CrisCrazy <criscrazy133@gmail.com>
2021-10-09 19:16:59 -0600

    v0.2

--- DevForm2/settings.py
+++ DevForm2/settings.py
@@ -74,11 +74,26 @@ WSGI_APPLICATION = 'DevForm2.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+"""
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'Ferreteria',
+        'USER': 'CrisCrazy',
+        'PASSWORD': '1313',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
+}
+"""
+
+import dj_database_url
+from decouple import config
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
 }
 
 
